c05e244ede231a7fad93f46ee8cf5863738cfab4
akiayane <67632960+akiayane@users.noreply.github.com>
2021-05-24 00:44:37 +0600

    upd

--- fast.go
+++ fast.go
@@ -1,14 +1,15 @@
 package assignment2
 
 import (
-	json "encoding/json"
-	easyjson "github.com/mailru/easyjson"
-	jlexer "github.com/mailru/easyjson/jlexer"
-	jwriter "github.com/mailru/easyjson/jwriter"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/mailru/easyjson"
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
 	"io"
-	"io/ioutil"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -19,32 +20,52 @@ func FastSearch(out io.Writer) {
 	if err != nil {
 		panic(err)
 	}
-
-	fileContents, err := ioutil.ReadAll(file)
-	if err != nil {
-		panic(err)
-	}
-
 	defer file.Close()
 
-	r := regexp.MustCompile("@")
-	seenBrowsers := []string{}
-	uniqueBrowsers := 0
-	foundUsers := ""
+	seenBrowsers := make(map[string]bool, 150)
+	foundUsers := new(bytes.Buffer)
 
-	lines := strings.Split(string(fileContents), "\n")
+	users := make([]User, 0)
 
-	users := make([]map[string]interface{}, 0)
-	for _, line := range lines {
-		user := User{}
-		if err != nil {
-			panic(err)
-		}
-		users = append(users, user)
+	scanner := bufio.NewScanner(file)
+
+	user_buffer := User{}
+
+	for scanner.Scan() {
+		user_buffer.UnmarshalJSON(scanner.Bytes())
+		users = append(users, user_buffer)
+		user_buffer = User{}
 	}
+
+	for i, user := range users {
+		isAnd := false
+		isMSIE := false
+
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android"){
+				isAnd = true
+
+				seenBrowsers[browser] = true
+
+			} else if strings.Contains(browser, "MSIE"){
+				isMSIE = true
+
+				seenBrowsers[browser] = true
+
+			}
+
+		}
+
+		if isAnd && isMSIE{
+			email := strings.Replace(user.Email, "@", " [at] ", -1)
+			foundUsers.WriteString(fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email))
+		}
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers.String())
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
 //----------------------------------------------------------------------------------------------------------------------
-
 // suppress unused package warning
 var (
 	_ *json.RawMessage
@@ -115,60 +136,11 @@ func easyjson9e1087fdDecodeAssignment2Ezz(in *jlexer.Lexer, out *User) {
 		in.Consumed()
 	}
 }
-func easyjson9e1087fdEncodeAssignment2Ezz(out *jwriter.Writer, in User) {
-	out.RawByte('{')
-	first := true
-	_ = first
-	{
-		const prefix string = ",\"browsers\":"
-		out.RawString(prefix[1:])
-		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
-			out.RawString("null")
-		} else {
-			out.RawByte('[')
-			for v2, v3 := range in.Browsers {
-				if v2 > 0 {
-					out.RawByte(',')
-				}
-				out.String(string(v3))
-			}
-			out.RawByte(']')
-		}
-	}
-	{
-		const prefix string = ",\"email\":"
-		out.RawString(prefix)
-		out.String(string(in.Email))
-	}
-	{
-		const prefix string = ",\"name\":"
-		out.RawString(prefix)
-		out.String(string(in.Name))
-	}
-	out.RawByte('}')
-}
-
-// MarshalJSON supports json.Marshaler interface
-func (v User) MarshalJSON() ([]byte, error) {
-	w := jwriter.Writer{}
-	easyjson9e1087fdEncodeAssignment2Ezz(&w, v)
-	return w.Buffer.BuildBytes(), w.Error
-}
-
-// MarshalEasyJSON supports easyjson.Marshaler interface
-func (v User) MarshalEasyJSON(w *jwriter.Writer) {
-	easyjson9e1087fdEncodeAssignment2Ezz(w, v)
-}
 
 // UnmarshalJSON supports json.Unmarshaler interface
 func (v *User) UnmarshalJSON(data []byte) error {
 	r := jlexer.Lexer{Data: data}
 	easyjson9e1087fdDecodeAssignment2Ezz(&r, v)
 	return r.Error()
-}
-
-// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
-func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
-	easyjson9e1087fdDecodeAssignment2Ezz(l, v)
 }
 
